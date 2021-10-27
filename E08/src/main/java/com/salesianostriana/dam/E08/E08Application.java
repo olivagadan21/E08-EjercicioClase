1fd488a6a4e10a96fbabadb76e1f229fb58b481c
Daniel Oliva García <oliva.gadan21@triana.salesianos.edu>
2021-10-27 13:33:02 +0200

    Commit en clase

---
+++ E08/src/main/java/com/salesianostriana/dam/E08/E08Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.E08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E08Application.class, args);
+	}
+
+}
---
+++ E08/src/main/java/com/salesianostriana/dam/E08/model/ClienteCorporativo.java
@@ -0,0 +1,4 @@
+package com.salesianostriana.dam.E08.model;
+
+public class ClienteCorporativo {
+}
